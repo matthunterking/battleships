250ed7b3b3246965ceec5d2ea0f776ec7792054d
matthunterking <matthew85king@gmail.com>
2021-10-15 11:26:50 +0100

    Started work on randomly placing ships on the grid (with overlap)

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const gameBoard = {
-  0: [{ value: 'sea', visable: false }, { value: 'right', visable: true, playerMove: 'right' }, { value: 'left', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'circle', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'up', visable: false }, { value: 'sea', visable: false }],
-  1: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'middle', visable: false }, { value: 'sea', visable: false }],
-  2: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'middle', visable: false }, { value: 'sea', visable: false }],
-  3: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'down', visable: false }, { value: 'sea', visable: false }],
+  0: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }],
+  1: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }],
+  2: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }],
+  3: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }],
   4: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }],
   5: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }],
   6: [{ value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }, { value: 'sea', visable: false }],
@@ -13,10 +13,95 @@ const gameBoard = {
 
 const squareOptions = ['sea', 'right', 'left', 'up', 'down', 'middle', 'circle', 'flag'];
 
-console.log('gameBoard', gameBoard);
+
 
 
 const $grid = document.querySelector('.grid');
+
+const ships = [
+  ['end', 'middle', 'middle', 'end'],
+  ['end', 'middle', 'end'],
+  ['end', 'middle', 'end'],
+  ['end', 'end'],
+  ['end', 'end'],
+  ['end', 'end'],
+  ['circle'],
+  ['circle'],
+  ['circle'],
+  ['circle']
+];
+
+const generateShipsToBePlaced = () => {
+  return ships.map(shipMakeUp => {
+    if (shipMakeUp.length > 1) {
+      const isVertical = Math.random() > 0.5 && shipMakeUp.length > 1;
+      const ship = shipMakeUp.map((shipPart, index) => {
+        if (shipPart === 'end') {
+          if (index === 0) {
+            return isVertical ? 'up' : 'left';
+          } else {
+            return isVertical ? 'down' : 'right';
+          }
+        }
+        return shipPart;
+      });
+      return { isVertical, ship };
+    }
+
+    return { isVertical: false, ship: shipMakeUp };
+  });
+};
+
+const getStartPointRange = ({ isVertical, ship }) => {
+  const maxPosition = 9 - (ship.length - 1);
+  const horizonalMax = isVertical ? 9 : maxPosition;
+  const verticalMax = isVertical ? maxPosition : 9;
+  return { horizonalMax, verticalMax };
+};
+
+const generateRandomPoint = (maximum) => {
+  return Math.floor(Math.random() * (maximum - 0 + 1) + 0);
+}
+
+const checkSquareIsAvailable = (horizonalCoordinate, verticalCoordinate) => {
+  //TO DO
+}
+
+const getStartPoint = (range, shipInfo) => {
+  const startPointHorizontal = generateRandomPoint(range.horizonalMax);
+  const startPointVertical = generateRandomPoint(range.verticalMax);
+}
+
+const generateShipLocations = () => {
+  const shipsToBePlaced = generateShipsToBePlaced();
+  shipsToBePlaced.forEach(shipInfo => {
+    const range = getStartPointRange(shipInfo);
+
+    const startPointHorizontal = Math.floor(Math.random() * (range.horizonalMax - 0 + 1) + 0);
+    const startPointVertical = Math.floor(Math.random() * (range.verticalMax - 0 + 1) + 0);
+    console.log('shipInfo', shipInfo);
+
+    // check if points are available
+
+    shipInfo.ship.forEach((part, index) => {
+      const verticalPosition = shipInfo.isVertical ? startPointVertical + index : startPointVertical;
+      const horizontalPosition = shipInfo.isVertical ? startPointHorizontal : startPointHorizontal + index;
+      console.log('part', part, verticalPosition, horizontalPosition);
+
+      gameBoard[verticalPosition] = gameBoard[verticalPosition].map((square, index) => {
+        if (index === horizontalPosition) {
+          return { value: part, visable: true };
+        } else {
+          return square;
+        }
+      });
+    })
+
+    console.log(shipInfo, range, startPointHorizontal, startPointVertical);
+    console.log('gameBoard', gameBoard);
+  });
+  console.log(shipsToBePlaced);
+};
 
 const clickSquare = ($target, rowNumber, columnNumber) => {
   const previousSquareValue = gameBoard[rowNumber][columnNumber].playerMove;
@@ -106,6 +191,7 @@ const checkAnswer = () => {
     document.querySelector('.winnerMessage').classList.remove('hidden');
   }
 };
+generateShipLocations();
 
 setUpGrid();
 
